ed4ba6daf793d89036b76362397e4fc0071283d0
juansanzone <juan.sanzone@gmail.com>
2014-02-22 13:14:35 -0300

    Initial Commit

---
+++ Peekme/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Peekme
+//
+//  Created by Juan on 22/02/14.
+//  Copyright (c) 2014 JSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Peekme/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Peekme
+//
+//  Created by Juan on 22/02/14.
+//  Copyright (c) 2014 JSoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
